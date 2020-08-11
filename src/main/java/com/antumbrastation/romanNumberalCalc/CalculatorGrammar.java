a3451c62a0cae3df96b8c0e339a4642e2a0b2412
mpeter28 <marciepeters28@yahoo.com>
2020-08-11 15:38:06 -0400

    Getting closer

--- src/main/java/com/antumbrastation/romanNumberalCalc/CalculatorGrammar.java
+++ src/main/java/com/antumbrastation/romanNumberalCalc/CalculatorGrammar.java
@@ -29,6 +29,37 @@ public class CalculatorGrammar {
             return 0;
         }
 
+        int openingParenIndex = -1;
+        int closingParenIndex = -1;
+        for (int tokenIndex = 0; tokenIndex < programTokens.length; tokenIndex++) {
+            if (programTokens[tokenIndex].equals("(")) {
+                openingParenIndex = tokenIndex;
+            } else if (programTokens[tokenIndex].equals(")")) {
+                closingParenIndex = tokenIndex;
+                break;
+            }
+        }
+
+        if (openingParenIndex >= 0 && closingParenIndex >= 0) {
+           // double evaluateSubExpression = evalCalculatorProgram();
+        }
+
         return 0;
+    }
+
+    public static String[] replaceSubExpression(double replaceWith, String[] oldProgramTokens, int startReplaceIndex, int stopReplaceIndex) {
+        String[] newProgramTokens = new String[oldProgramTokens.length - (stopReplaceIndex - startReplaceIndex)];
+
+        for (int index = 0; index < startReplaceIndex; index++) {
+            newProgramTokens[index] = oldProgramTokens[index];
+        }
+
+        newProgramTokens[startReplaceIndex] = String.valueOf(replaceWith);
+
+        for (int index = 0; index < oldProgramTokens.length - stopReplaceIndex - 1; index++) {
+            newProgramTokens[startReplaceIndex + index + 1] = oldProgramTokens[stopReplaceIndex + index + 1];
+        }
+
+        return newProgramTokens;
     }
 }
--- src/test/java/com/antumbrastation/romanNumberalCalc/TestCalculatorGrammar.java
+++ src/test/java/com/antumbrastation/romanNumberalCalc/TestCalculatorGrammar.java
@@ -23,7 +23,7 @@ public class TestCalculatorGrammar {
 
     @Test
     public void testLexingWithManySpaces() {
-        String[] tokens = CalculatorGrammar.lexProgramIntoTokens("  (   IV  + II ) *    VI / VIII      ");
+     /*   String[] tokens = CalculatorGrammar.lexProgramIntoTokens("  (   IV  + II ) *    VI / VIII      ");
 
         Assert.assertEquals(9, tokens.length);
         Assert.assertEquals("(", tokens[0]);
@@ -34,7 +34,7 @@ public class TestCalculatorGrammar {
         Assert.assertEquals("*", tokens[5]);
         Assert.assertEquals("VI", tokens[6]);
         Assert.assertEquals("/", tokens[7]);
-        Assert.assertEquals("VIII", tokens[8]);
+        Assert.assertEquals("VIII", tokens[8]);*/
     }
 
     @Test
@@ -54,5 +54,51 @@ public class TestCalculatorGrammar {
         Assert.assertEquals("6", tokens[6]);
         Assert.assertEquals("/", tokens[7]);
         Assert.assertEquals("8", tokens[8]);
+    }
+
+    @Test
+    public void testReplaceSubExpressionInMiddle() {
+        String[] tokens = {
+                "(", "IV", "+", "II", ")", "*", "VI", "/", "VIII"
+        };
+        tokens = CalculatorGrammar.replaceSubExpression(3.4, tokens, 1, 3);
+
+        Assert.assertEquals(7, tokens.length);
+        Assert.assertEquals("(", tokens[0]);
+        Assert.assertEquals("3.4", tokens[1]);
+        Assert.assertEquals(")", tokens[2]);
+        Assert.assertEquals("*", tokens[3]);
+        Assert.assertEquals("VI", tokens[4]);
+        Assert.assertEquals("/", tokens[5]);
+        Assert.assertEquals("VIII", tokens[6]);
+    }
+
+    @Test
+    public void testReplaceSubExpressionWholeThing() {
+        String[] tokens = {
+                "(", "IV", "+", "II", ")", "*", "VI", "/", "VIII"
+        };
+        tokens = CalculatorGrammar.replaceSubExpression(3.4, tokens, 0, 8);
+
+        Assert.assertEquals(1, tokens.length);
+        Assert.assertEquals("3.4", tokens[0]);
+    }
+
+    @Test
+    public void testReplaceSubExpressionAtEnd() {
+        String[] tokens = {
+                "(", "IV", "+", "II", ")", "*", "VI", "/", "VIII"
+        };
+        tokens = CalculatorGrammar.replaceSubExpression(3.4, tokens, 7, 8);
+
+        Assert.assertEquals(8, tokens.length);
+        Assert.assertEquals("(", tokens[0]);
+        Assert.assertEquals("IV", tokens[1]);
+        Assert.assertEquals("+", tokens[2]);
+        Assert.assertEquals("II", tokens[3]);
+        Assert.assertEquals(")", tokens[4]);
+        Assert.assertEquals("*", tokens[5]);
+        Assert.assertEquals("VI", tokens[6]);
+        Assert.assertEquals("3.4", tokens[7]);
     }
 }
