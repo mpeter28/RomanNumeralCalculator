bbf5d6320cf53149c9413a3f7e6fa272ae141cfe
mpeter28 <marciepeters28@yahoo.com>
2020-08-11 16:36:17 -0400

    Whip up a command line

--- src/main/java/com/antumbrastation/romanNumberalCalc/CmdLine.java
+++ src/main/java/com/antumbrastation/romanNumberalCalc/CmdLine.java
@@ -1,4 +1,15 @@
 package com.antumbrastation.romanNumberalCalc;
 
 public class CmdLine {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.exit(1);
+        }
+
+        String[] tokens = CalculatorGrammar.lexProgramIntoTokens(args[0]);
+        tokens = CalculatorGrammar.replaceRomanTokensWithArabicTokens(tokens);
+
+        System.out.println(CalculatorGrammar.evalCalculatorProgram(tokens));
+    }
 }
